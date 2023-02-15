ce93c4057c60e42ad08e1eae3c920de78644e9de
Dev-organico <joao.zolet@gmail.com>
2023-02-15 20:12:59 -0300

    entrega

--- groupBy.sql
+++ groupBy.sql
@@ -10,3 +10,12 @@ SELECT "userId" AS id, COUNT("userId") AS education FROM educations GROUP BY "us
 //Q3
 
 
+
+
+//Q4
+
+SELECT MAX(jobs.salary) AS "maximumSalary", roles.name AS "roles"
+FROM roles
+JOIN jobs ON roles."id" = jobs."roleId"
+GROUP BY roles."id"
+ORDER BY "maximumSalary";
